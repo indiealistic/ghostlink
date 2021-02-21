cd3b1f2a6a81c169d931f27906369add02469431
Indiealistic <hey@indiealistic.studio>
2021-02-22 00:41:09 +0700

    Add progress circle

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,8 +2,54 @@ import ghostlink from 'source/ghostlink.js';
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+let progress = document.querySelector('.progress');
+let circle = document.querySelector('.progress-circle');
+let length = circle.getTotalLength();
+let timeline = gsap.timeline();
+
+gsap.set(progress, {
+  strokeDasharray: length,
+  strokeDashoffset: length,
+});
+
 ghostlink.init({
   on: 'a:not(.github-button)',
+  await(resolve) {
+    timeline.clear().to(progress, {
+      rotation: 360,
+      strokeDashoffset: 0,
+      strokeDasharray: length,
+      duration: 1.2,
+      onComplete: () => {
+        resolve();
+      },
+    }).to(progress, {
+      opacity: 1,
+      duration: 0.4,
+    }, '<');
+  },
+  onCancel() {
+    timeline.clear().to(progress, {
+      opacity: 0,
+      rotation: 0,
+      strokeDashoffset: length,
+      duration: 0.4,
+    });
+  },
+  onDispatch() {
+    timeline.clear().to(progress, {
+      opacity: 0,
+      duration: 0.2,
+      delay: 0.1,
+      onComplete: () => {
+        gsap.set(progress, {
+          rotation: 0,
+          strokeDasharray: length,
+          strokeDashoffset: length,
+        });
+      },
+    });
+  },
 });
 
 barba.hooks.enter(() => {
@@ -17,13 +63,22 @@ barba.init({
     leave(data) {
       return gsap.to(data.current.container, {
         opacity: 0,
-        duration: 0.4,
+        duration: 0.25,
       });
     },
     enter(data) {
       return gsap.from(data.next.container, {
         opacity: 0,
-        duration: 0.4,
+        duration: 0.25,
+      });
+    },
+  }],
+  views: [{
+    namespace: 'index',
+    afterEnter() {
+      gsap.to(document.querySelector('.github-button'), {
+        opacity: 1,
+        delay: 0.25,
       });
     },
   }],
